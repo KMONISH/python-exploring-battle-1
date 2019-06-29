c0821dbac8d5498c1a3d81e55ab0af8e12a891bd
KMONISH <monishkb1999@gmail.com>
2019-06-29 13:48:39 -0500

    pri_sort

---
+++ pri_sort.py
@@ -0,0 +1,11 @@
+into = [13,56,34,58,52,93,35,24]
+print(into)
+print("\sorted is =",
+sorted(into))
+print("\maximum is = ",
+max(into))
+print("\minimum is =",
+min(into))
+print("\length is = ",
+len(into))
+
